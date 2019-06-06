c14ebb46e4d3aed70b5f8ff118799256b91881c0
irt_paulo <luis.oliveira@reconcavo.org.br>
2019-06-06 12:58:04 -0300

    Criação de classe Helper para isolar a responsabilidade de conversão de string para Date e vice-versa por meio de métodos estáticos. Ainda foi implementada no construtor uma proteção contra a sua instanciação, que lança um erro para impedir a tentativa e exibir um alerta ao desenvolvedor que tentou realizar este processo.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,20 +9,17 @@ class NegociacaoController {
     }
 
     adicionar(event) {
+
         event.preventDefault(); 
-        
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice) =>  item - indice % 2)
-        );
+        let helper = new DateHelper();
 
         let negociacao = new Negociacao(
-            data,
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
         console.log(negociacao);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 }
---
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,14 @@
+class DateHelper {
+
+    constructor() {
+        throw new Error('DateHelper não pode ser instanciado.');
+    }
+
+    static dataParaTexto(data) {
+        return data.getDate() + '/' + (data.getMonth() + 1) + '/'  + data.getFullYear();
+    }
+
+    static textoParaData(texto) { 
+        return new Date(...texto.split('-').map((item, indice) =>  item - indice % 2));
+    }
+}
