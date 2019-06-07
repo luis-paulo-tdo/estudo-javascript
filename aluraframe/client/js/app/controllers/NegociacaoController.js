448a6f4bd2b7c895c5156f224c894c7350aa6863
irt_paulo <luis.oliveira@reconcavo.org.br>
2019-06-07 13:16:05 -0300

    Criação da classe ListaNegociacoes para encapsular o array que será utilizado para adicionar novas negociações e exibí-las na interface gráfica. Através da programação defensiva foi possível blindar o array de operações de atualização e exclusão programando o seu getter para devolver apenas uma cópia de seus valores. Enquanto uma cópia pode ser alterada ou excluída, o array privado permance intacto dentro da classe.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,20 +6,32 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
-    adicionar(event) {
+    adiciona(event) {
 
         event.preventDefault(); 
-        let helper = new DateHelper();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+ 
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
 
-        console.log(negociacao);
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade = 1;
+        this._inputValor = 0.0;
+
+        this._inputData.focus();
     }
 }
